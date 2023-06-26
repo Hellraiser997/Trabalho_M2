13fbba9810c6f6c4a03433876be46c2233e7bc5a
Hellraiser997 <felipesg03@gmail.com>
2023-06-26 18:11:54 -0300

    Adicionado divs no HTML e consts de unidades e referencias de DOM no JS

---
+++ script.js
@@ -0,0 +1,10 @@
+//Unidades de cada grandeza
+const opcoesUnidades = {
+    temperatura: ["Celsius", "Fahrenheit", "Kelvin"],
+    comprimento: ["Metro", "Centímetro", "Pé", "Polegada"],
+    massa: ["Quilograma", "Grama", "Libra", "Onça"]
+}
+
+const selectUnidadePincipal = document.getElementById("unidadePrincipal");
+const selectUnidadeConversao = document.getElementById("unidadeConversao");
+const selectConversao = document.getElementById("conversao")
